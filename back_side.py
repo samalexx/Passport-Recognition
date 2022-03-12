a96f5c1d307e10a9c779978157c0917a33b3f3b3
p0wned17 <bilaloff.art@gmail.com>
2022-03-12 23:37:44 +0500

    fix bugs with back side and fix regular

--- back_side.py
+++ back_side.py
@@ -12,13 +12,11 @@ async def side_main(contents):
 
     image = np.array(Image.open(io.BytesIO(contents)))
 
-    crop_image = find_rectangle(image)
-    
-    rectangles_contour, table = draw_contours(crop_image)
+    image = cv2.resize(image, (790, 499))
 
-    cv2.imshow('s',table)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    crop_image = find_rectangle(image)
+
+    rectangles_contour, table = draw_contours(crop_image)
 
     text = recognize_text(rectangles_contour, table)
 
@@ -33,7 +31,9 @@ def find_rectangle(img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         image_blur = cv2.GaussianBlur(gray, (3,3), 1)
 
-        obr_image = cv2.Canny(image_blur, 100, 300, 4)
+        obr_image = cv2.Canny(image_blur, 75, 280, 3)
+
+
 
         contours, _ = cv2.findContours(obr_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)   
 
@@ -52,6 +52,7 @@ def find_rectangle(img):
 # contour find and draw in input image
 def draw_contours(crop):
     img = crop[40:120, 0:350]
+
     table = correct_skew1(img, 0.2, 10)
 
     table = cv2.resize(table, (500,300))
@@ -91,6 +92,9 @@ def recognize_text(rectangles_contour, table):
         if w > 100 and h > 10 and 20 < y < 200 and x < 300:
             idx += 1
             cv2.rectangle(table, (x, y), (x + w, y + h), (36,255,12), 2)
+            cv2.imshow('s',table)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
             # cv2.putText(table, f'{idx}', (x,y), cv2.FONT_HERSHEY_COMPLEX, 0.7, (255,255,255), 2)
             new_image = table[y-2:y+h+2, x-2:x+w+2]
             new_image = correct_skew1(new_image, 0.5, 5)
@@ -103,30 +107,35 @@ def recognize_text(rectangles_contour, table):
             custom_config = r'-l rus+frn --psm 6 --oem 1'
             text = pytesseract.image_to_string(invert, config=custom_config)
             text = text.replace("\n", '')
+            print(text)
             text_ocr.append(text.capitalize())
+    print(text_ocr)
     return text_ocr
     
 
 def text_correction(text, img):
     text_to_Str = str(text)
-    text_without_symbol = re.sub(r"[^\d\.\s\,BВ]", '', text_to_Str)
+    text_without_symbol = re.sub(r"[^\d\.\s\,BВв]", '', text_to_Str)
     category_date = str(re.findall(r"[В|В|в]{1}\W+\d{2}\.\d{2}.\d{4}", text_without_symbol))  
     category_date_sub = re.sub(r"[\[\]|,|']",'', category_date)
-    if not category_date_sub.strip():
+    print(category_date_sub)
+    if category_date_sub != str(re.match(r"[В|В|в]{1}\W+\d{2}\.\d{2}.\d{4}", category_date_sub)):
+        text.reverse()
+        text_to_str = str(text)
+        text_without_symbol1 = re.sub(r"[^\d\.\s\,BВ]", '', text_to_str)
+        category_date1 = str(re.findall(r"[В|В|в]{1}\W+\d{2}\.\d{2}.\d{4}", text_without_symbol1))  
+        category_date_sub1 = re.sub(r"[\[\]|,|']",'', category_date1)
+        data = {
+            "Category and date": category_date_sub1
+        }
+        if not category_date_sub1.strip():
+            data = kat_is_zero(img)
+            return data 
+    elif not category_date_sub.strip():
         data = kat_is_zero(img)
-        return data
-    else: 
-        if category_date_sub == str(re.match(r"[В|В|в]{1}\W+\d{2}\.\d{2}.\d{4}", category_date_sub)):
-            text.reverse()
-            text_to_str = str(text)
-            text_without_symbol1 = re.sub(r"[^\d\.\s\,BВ]", '', text_to_str)
-            category_date1 = str(re.findall(r"[В|В|в]{1}\W+\d{2}\.\d{2}.\d{4}", text_without_symbol1))  
-            category_date_sub1 = re.sub(r"[\[\]|,|']",'', category_date1)
-            data = {
-                "Category and date": category_date_sub1
-            }
-        else:
-            data = {
-            "Category and date": category_date_sub
-            }
+        return data 
+    else:
+        data = {
+        "Category and date": category_date_sub
+        }
     return data
